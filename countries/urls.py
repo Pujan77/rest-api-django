0c671c631e308bbb568bcf38227b5eb803583a1e
Pujan77 <pujansapkota7@gmail.com>
2022-06-01 15:17:53 +0545

    create model and views

---
+++ countries/urls.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+from countries import views
+
+urlpatterns = [
+    re_path(r'^api/countries$', views.countries_list),
+    re_path(r'^api/countries/(?P<pk>[0-9]+)$', views.countries_detail)
+
+]
